803a0bc999c0b973985bee5fe564ef418463e4d1
GusGit2003 <gusavegue@gmail.com>
2024-03-10 14:04:45 -0600

    Solucion del bug de la tabla de puntajes

--- puntajes.js
+++ puntajes.js
@@ -1,46 +1,43 @@
-var usuarios = localStorage.getItem("usuarios"); //Leer usuarios del localStorage, al inicio no existe
+var usuarios = localStorage.getItem("usuarios");
 var tiempos = [];
-
-if(usuarios==null) usuarios=[]; //Si es nulo creamos arreglo vacío
-else usuarios=JSON.parse(usuarios); //Recibe una cadena JSON y en base a esta cadena construye un objeto
+if (usuarios == null) usuarios = []; //Si es nulo creamos arreglo vacío
+else usuarios = JSON.parse(usuarios);  //Recibe una cadena JSON y en base a esta cadena construye un objeto
 console.log(usuarios);
 
-function mostrar(){
-    document.getElementById("clasificacion").innerHTML="";
-    var tabla="<tr> <th>Nombre</th> <th>Puntaje</th> <th>Tiempo</th> </tr>";
-
-    /*
-    usuarios.sort(function(a, b) {
-        return a.tiempo - b.tiempo;
-    });
-    console.log(usuarios);
-    */
+function mostrar() {
+    document.getElementById("clasificacion").innerHTML = "";
+    var tabla = "<tr> <th>Nombre</th> <th>Puntaje</th> <th>Tiempo</th> </tr>";
 
     for(var i in usuarios){
         var usuario = JSON.parse(usuarios[i]);
-        tiempos[i] = usuario.tiempo;
+
+        if(!tiempos.hasOwnProperty(usuario.tiempo)) {
+            tiempos[usuario.tiempo] = [];
+        }
+        //La clave es el tiempo y el valor es un arreglo con los usuarios que tienen ese tiempo
+        tiempos[usuario.tiempo].push(usuario);
     }
-    tiempos.sort();
-    console.log(tiempos);
 
-    for(var i=0; i<tiempos.length; i++){
-        for(var j=0; j<usuarios.length; j++) {
-            var usuario = JSON.parse(usuarios[j]);
+    //Se extrae todas las claves (tiempos) y los ordena de menor a mayor
+    var tiemposOrden = Object.keys(tiempos).sort((a, b) => a - b);
 
-            if(tiempos[i] === usuario.tiempo){
-                tabla += "<tr>";
-                tabla += "<td>" + usuario.nombre + "</td>";
-                tabla += "<td>" + usuario.puntaje + "</td>";
-                tabla += "<td>" + usuario.tiempo + "</td>";
-                tabla += "</tr>";
-                break;
-            }
+    for(var i=0; i<tiemposOrden.length; i++){
+        var tiempo = tiemposOrden[i];
+        var usuariosConEsteTiempo = tiempos[tiempo];
+
+        for(var j=0; j<usuariosConEsteTiempo.length; j++) {
+            var usuario = usuariosConEsteTiempo[j];
+            tabla += "<tr>";
+            tabla += "<td>"+usuario.nombre+"</td>";
+            tabla += "<td>"+usuario.puntaje+"</td>";
+            tabla += "<td>"+usuario.tiempo+"</td>";
+            tabla += "</tr>";
         }
     }
-    
+
     document.getElementById("clasificacion").innerHTML = tabla;
 }
 
-window.onload = function(){
+window.onload = function () {
     mostrar();
 }
